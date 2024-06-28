1f98ac6b8dd2c329443175b928daa6bf0dcd4f5a
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2024-06-28 09:11:03 -0400

    Add files via upload

---
+++ x/src/js-css/script.js
@@ -0,0 +1,26 @@
+function toggleChapters(bookElement) {
+    const chapters = bookElement.querySelector('.chapters');
+    const allBooks = document.querySelectorAll('.book .chapters');
+
+    allBooks.forEach(chapter => {
+        if (chapter !== chapters) {
+            chapter.style.display = 'none';
+        }
+    });
+
+    chapters.style.display = chapters.style.display === 'block' ? 'none' : 'block';
+}
+
+function loadChapterContent(filePath) {
+    const contentDiv = document.getElementById('chapter-content');
+    
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            contentDiv.textContent = data;
+        })
+        .catch(error => {
+            contentDiv.textContent = 'Error cargando el capítulo';
+            console.error('Error cargando el capítulo:', error);
+        });
+}
