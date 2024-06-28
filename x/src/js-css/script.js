dcf84973f34ebd4d7047ae83df6ae88037cd6e9f
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2024-06-28 10:11:51 -0400

    Update script.js

--- x/src/js-css/script.js
+++ x/src/js-css/script.js
@@ -1,16 +1,16 @@
 function toggleChapters(bookElement) {
     const chapters = bookElement.querySelector('.chapters');
-    
+
     // Oculta todos los capítulos abiertos
     document.querySelectorAll('.chapters').forEach(chapterDiv => {
         if (chapterDiv !== chapters) {
             chapterDiv.style.display = 'none';
         }
     });
-    
+
     // Alterna la visibilidad del capítulo del libro seleccionado
     if (chapters.style.display === 'none' || chapters.style.display === '') {
-        chapters.style.display = 'block';
+        chapters.style.display = 'flex'; // Asegurarse de que sea flex para mantener la estructura horizontal
     } else {
         chapters.style.display = 'none';
     }
